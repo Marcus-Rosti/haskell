b6e7a13c660dfb232c1d2dea0b58f65999c32e80
Marcus Rosti <marcus.rosti@axiosengineering.com>
2014-10-14 11:26:49 -0400

    fuck yeah, detection

--- crypto/xorCipher.hs
+++ crypto/xorCipher.hs
@@ -4,9 +4,13 @@ import Data.List
 import Data.List.Split
 import qualified Data.ByteString.Char8 as B
 
+
+
 -- TO CRACK
 master_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-master_key = ' '
+master_key = 'G'
+
+freqDict = ["the", "and", "for", "are", "but", "not", "you", "all", "any", "can", "had", "her", "was", "one", "our", "out", "day", "get", "has", "him", "his", "how", "man", "new", "now", "old", "see", "two", "way", "who", "boy", "did", "its", "let", "put", "say", "she", "too", "use"]
 
 hexToDigit = map digitToInt
 
@@ -16,22 +20,31 @@ hexByte hex = [digitToHex (shiftR hex 4)] ++ [digitToHex (hex .&. 15)]
 xorDigit (x,y) = xor x y
 xorBytes x y = xor x y
 
-
 hexToPairDigit s = chunksOf 2 $ map digitToInt s 
 combine [x,y] = shiftL x 4 + y
 combineAllDigs digits = map combine digits
 combineAllStrs string = map combine (hexToPairDigit string)
+
+unionList:: [Bool] -> Bool
+unionList [] = False
+unionList (x:[]) = x
+unionList (x:xs) = x || (unionList xs)
+
+comoIngles:: [Char] -> Bool
+comoIngles charArray = unionList [isInfixOf key charArray | key <- freqDict]
 
 decrypt l k = result
   where
     decrypted = map (\ e -> xor e k) l
     result = map chr decrypted
 
--- fixedCharXOR str char = result 
--- 	where 
--- 		nibbles = map xorDigit (zip (replicate (length str) (combine.hexToPairDigit) char)) (hexToPairDigit str))
--- 		hex = nibbles --map digitToHex nibbles
--- 		result =  hex
-
 main = do
-	print $ decrypt (combineAllStrs master_string) (ord master_key)
+	let decrypted = decrypt (combineAllStrs master_string) (ord master_key)
+	print $ comoIngles decrypted
+
+
+
+
+
+
+
