2f4794605dd6f0353ba76c005e9850f593b04ae0
Marcus Rosti <marcus.rosti@axiosengineering.com>
2014-11-03 10:03:00 -0500

    starting to get more interesting

---
+++ euler/prob_014/longColl.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+--hella slow!
+--Memoization would be best but I don't want to deal with
+--the overhead of that right now
+--TODO:: make this faster
+collatzSize (num, size)
+	| num == 1 = size
+	| (mod) num 2 == 0 = collatzSize (quot num 2, size+1)
+	| otherwise = collatzSize (3 * num + 1, size+1)
+
+main = do
+	let nums = zip [1..999999] [0,0..]
+	let collNums = map collatzSize nums
+	print $ map (+1) (findIndices (== (maximum collNums) ) (collNums))
+
---
+++ euler/prob_034/digFact.hs
@@ -0,0 +1,23 @@
+import Data.List
+import Data.Bits
+import Data.Maybe
+
+--Haskell wiki for fast Fibs
+--Did use bitsize but that's depri
+fib :: Int -> Integer
+fib n = snd . foldl' fib' (1, 0) . dropWhile not $
+            [testBit n k | k <- let s = finiteBitSize n in [s-1,s-2..0]]
+    where
+        fib' (f, g) p
+            | p         = (f*(f+2*g), ss)
+            | otherwise = (ss, g*(2*f-g))
+            where ss = f*f+g*g
+
+digitArray:: Integer -> [Int]
+digitArray 0 = []
+digitArray x = digitArray (div x 10) ++ [mod x 10]
+
+
+main = do
+	let fibs = [fib x | x <- [4..10], y <- digitArray x]
+	print fibs
