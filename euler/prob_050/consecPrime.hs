3d4f82dbe920b0c3d44198db10082955c0a5b4e9
Marcus Rosti <marcus.rosti@axiosengineering.com>
2014-11-06 14:52:39 -0500

    Binnet's formula worked great

--- euler/prob_050/consecPrime.hs
+++ euler/prob_050/consecPrime.hs
@@ -11,9 +11,9 @@ primes = 2 : oddprimes
 
 primeFactors n = factor n primes
   where
-    factor n (p:ps) 
+    factor n (p:ps)
         | p*p > n        = [n]
-        | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+        | n `mod`  p == 0 = p : factor (n `div` p) (p:ps)
         | otherwise      =     factor n ps
 --------------------------------------------------------------------------------
 
---
+++ euler/prob_100/prob.hs
@@ -0,0 +1,12 @@
+binnetsWork :: (Integer, Integer) -> Int -> (Integer, Integer)
+binnetsWork (b,n) 0 = (b,n)
+binnetsWork (b,n) 1 = (3 * b + 2 * n - 2,4 * b + 3 * n - 3)
+binnetsWork (b,n) i = binnetsWork (3 * b + 2 * n - 2,4 * b + 3 * n - 3) (i-1)
+
+binnets :: Int -> (Integer,Integer)
+binnets 0 = (15,21)
+binnets i = binnetsWork (3 * 15 + 2 * 21 - 2, 4 * 15 + 3 * 21 - 3) (i-1)
+
+
+main = do
+  print $ take 10 $ dropWhile (\(f,s) -> s <10^12) $ map binnets [0..]
