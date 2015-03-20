8289c7dd7f32f885dfddc368c89353aeec5ed9f6
Marcus Rosti <marcus.rosti@axiosengineering.com>
2015-03-20 07:36:58 -0400

    csv parsing

--- data-science/ch1/Main.hs
+++ data-science/ch1/Main.hs
@@ -1,14 +1,45 @@
 import Control.Exception (catch, SomeException)
 import System.Environment (getArgs)
 
+import Text.CSV
+
 main::IO()
-main=do
+main = csvFile
+
+txtFile :: IO()
+txtFile = do
   args <- getArgs
-  let filename = case args of
-    (a:_) -> a
-    _     -> "input.txt"
-  input <- readFile filename
+  let filename = parseArgs args
+  input <- catch (readFile filename) $ \err -> print (err::SomeException) >> return ""
   print $ countWords input
+
+csvFile :: IO()
+csvFile = do
+  let filename_csv = "input.csv"
+  input_csv <- readFile filename_csv
+  let csv = parseCSV filename_csv input_csv
+  either handleError dowork csv
+
+handleError _ = putStrLn "parsing error"
+dowork = print.findOldest.tail
+
+findOldest :: [Record] -> Record
+findOldest [] = []
+findOldest xs = foldl1 (\a b -> if age b > age a then b else a) xs
+
+age :: [String] -> Int
+age [] = 0
+age [a,_] = toInt a
+age [_] = 0
+
+toInt :: String -> Int
+toInt = read
+
+
+
+parseArgs :: [String] -> String
+parseArgs (a:_) = a
+parseArgs _ = "input.txt"
 
 countWords :: String -> [Int]
 countWords input = map (length.words) (lines input)
